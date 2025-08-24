86ef46f947ab7e3e7a171a3399949fba6c0449d5
talal4a <talalxus000@gmail.com>
2025-08-24 13:55:53 +0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+// use std::io;
+// fn sum(a: i32, b: i32) -> i32 {
+//     a + b
+// }
+// fn sub(a: i32,b: i32)-> i32{
+//     a-b
+// }
+// fn main() {
+//     let mut input_a = String::new();
+//     let mut input_b = String::new();
+//     println!("What is your first number...");
+//     io::stdin()
+//         .read_line(&mut input_a)
+//         .expect("Failed to read line");
+//     println!("What is your second number...");
+//     io::stdin()
+//         .read_line(&mut input_b)
+//         .expect("Failed to read line");
+//     let a: i32 = input_a.trim().parse().expect("Please type a number!");
+//     let b: i32 = input_b.trim().parse().expect("Please type a number!");
+//     let result = sum(a, b);
+//     println!("The sum is: {}", result);
+//     let subtract=sub(a, b);
+//     println!("The substract:{}",subtract);
+// }
+// fn main(){
+// let value:bool=false;
+// if value==true {
+//     println!("hello");
+// }
+// else {
+//     println!("goodbye");
+// }
+// }
+// fn main(){
+//     let value=5;
+    
+// if value>5 {
+//     println!(">5");
+// }
+// else if value<5 {
+//     println!("<5");
+// }
+// else if value==5 {
+//     println!("=5");
+// }
+// else{
+//     println!("Nothing found ");
+// }
+// }
+fn main(){
+    
 }
